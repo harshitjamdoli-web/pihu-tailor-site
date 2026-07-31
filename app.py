a92069ac2eb6ffabb94a2fbcda029d67a9a22cca
harshitjamdoli-web <harshitjamdoli@gmail.com>
2026-07-31 19:14:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("index.html")
+    return render_template("index.html")
+
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return """<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://pihu-the-master-tailor.onrender.com/</loc>
+        </url>
+    </urlset>"""
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
